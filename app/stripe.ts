1ca76b58651fb1f2d3e3f99a581f5e6e838b6f38
Jan-Georges Jersild Balin <jgbalin@gmail.com>
2024-10-15 19:51:47 +0200

    chore: env stuff

--- app/stripe.ts
+++ app/stripe.ts
@@ -1,4 +1,2 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-export const stripe = require("stripe")(
-  process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY
-);
+export const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
