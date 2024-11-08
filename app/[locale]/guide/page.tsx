2e8e7a39ebe8e8ed96e76b1471d6975dc42b9557
Jan-G Balin <jgbalin@gmail.com>
2024-11-08 22:30:58 +0100

    try see waht happens to return page now

--- app/[locale]/guide/page.tsx
+++ app/[locale]/guide/page.tsx
@@ -1,7 +1,17 @@
-export default function GuidePage() {
+import { getTranslations, setRequestLocale } from "next-intl/server";
+
+export default async function GuidePage({
+  params: { locale },
+}: {
+  params: { locale: string };
+}) {
+  setRequestLocale(locale);
+
+  const t = await getTranslations("Guide");
+
   return (
     <div className="w-full h-full flex justify-center pt-48">
-      <h1 className="text-4xl font-bold">Kommer snart</h1>
+      <h1 className="text-4xl font-bold">{t("title")}</h1>
     </div>
   );
 }
--- app/[locale]/return/page.tsx
+++ app/[locale]/return/page.tsx
@@ -1,60 +1,54 @@
 "use client";
+import { useTranslations } from "next-intl";
 import { useSearchParams } from "next/navigation";
 import { Suspense } from "react";
 
-const CheckoutReturn = () => {
-  return (
-    <div className="flex items-center justify-center min-h-screen w-full bg-gray-100 px-4 py-8">
-      <div className="bg-white p-6 md:p-8 rounded-lg shadow-md max-w-md w-full">
-        <Suspense fallback={<div>Loading...</div>}>
-          <ReturnRenderer />
-        </Suspense>
-      </div>
-    </div>
-  );
-};
-
-function ReturnRenderer() {
+export default function CheckoutReturn() {
   const searchParams = useSearchParams();
   const success = searchParams.get("success");
   const canceled = searchParams.get("canceled");
 
-  if (success) {
-    return (
-      <div className="text-center">
-        <h2 className="text-xl md:text-2xl font-bold mb-4">Ordre bekræftet!</h2>
-        <p className="text-gray-600 mb-6">
-          Mange tak for din bestilling. Du modtager en ordrebekræftelse via
-          e-mail.
-        </p>
-        <a
-          href="/"
-          className="inline-block bg-brandGreen text-white px-6 py-2 rounded hover:bg-green-600 transition"
-        >
-          Hjem
-        </a>
-      </div>
-    );
-  }
-  if (canceled) {
-    return (
-      <div className="text-center">
-        <h2 className="text-xl md:text-2xl font-bold mb-4">
-          Bestilling ikke gennemført
-        </h2>
-        <p className="text-gray-600 mb-6">
-          Betalingen blev afbrudt, prøv venligst igen.
-        </p>
-        <a
-          href="/"
-          className="inline-block bg-brandGreen text-white px-6 py-2 rounded hover:bg-green-600 transition"
-        >
-          Hjem
-        </a>
-      </div>
-    );
-  }
-  return null;
-}
+  const t = useTranslations("Return");
 
-export default CheckoutReturn;
+  return (
+    <div className="flex items-center justify-center min-h-screen w-full bg-gray-100 px-4 py-8">
+      <div className="bg-white p-6 md:p-8 rounded-lg shadow-md max-w-md w-full">
+        <Suspense fallback={<div>Loading...</div>}>
+          {success && (
+            <div className="text-center">
+              <h2 className="text-xl md:text-2xl font-bold mb-4">
+                {t("success")}
+              </h2>
+              <p className="text-gray-600 mb-6">
+                Mange tak for din bestilling. Du modtager en ordrebekræftelse
+                via e-mail.
+              </p>
+              <a
+                href="/"
+                className="inline-block bg-brandGreen text-white px-6 py-2 rounded hover:bg-green-600 transition"
+              >
+                Hjem
+              </a>
+            </div>
+          )}
+          {canceled && (
+            <div className="text-center">
+              <h2 className="text-xl md:text-2xl font-bold mb-4">
+                Bestilling ikke gennemført
+              </h2>
+              <p className="text-gray-600 mb-6">
+                Betalingen blev afbrudt, prøv venligst igen.
+              </p>
+              <a
+                href="/"
+                className="inline-block bg-brandGreen text-white px-6 py-2 rounded hover:bg-green-600 transition"
+              >
+                Hjem
+              </a>
+            </div>
+          )}
+        </Suspense>
+      </div>
+    </div>
+  );
+}
