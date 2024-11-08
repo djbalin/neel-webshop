b3126c45ceded133d119e72d3d57a0f7b1404808
Jan-Georges Jersild Balin <jgbalin@gmail.com>
2024-11-08 17:28:59 +0100

    feat: catch errs

--- i18n/request.ts
+++ i18n/request.ts
@@ -1,3 +1,4 @@
+import { IntlErrorCode } from "next-intl";
 import { getRequestConfig } from "next-intl/server";
 import { Locale } from "./config";
 import { routing } from "./routing";
@@ -14,5 +15,20 @@ export default getRequestConfig(async ({ requestLocale }) => {
   return {
     locale,
     messages: (await import(`../messages/${locale}.json`)).default,
+    onError(error) {
+      console.log("ERROR IDENTIFIED");
+
+      console.error(error);
+    },
+
+    getMessageFallback({ namespace, key, error }) {
+      const path = [namespace, key].filter((part) => part != null).join(".");
+
+      if (error.code === IntlErrorCode.MISSING_MESSAGE) {
+        return path + " is not yet translated";
+      } else {
+        return "Dear developer, please fix this message: " + path;
+      }
+    },
   };
 });
