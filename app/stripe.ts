f16e974aca77dd30801d9c6a0587cfffabca1474
Jan-Georges Jersild Balin <jgbalin@gmail.com>
2025-10-03 22:32:45 +0200

    log env

--- app/stripe.ts
+++ app/stripe.ts
@@ -2,6 +2,7 @@ import "server-only";
 
 import Stripe from "stripe";
 if (!process.env.STRIPE_SECRET_KEY) {
+  console.log(process.env);
   throw new Error("STRIPE_SECRET_KEY is not set");
 }
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
